a1e6889bcce8694bbdbaf44d6fcafb7d7cff453a
n1ckfg <nick@fox-gieg.com>
2018-05-07 16:42:54 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -14,6 +14,27 @@ done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 PROJECT=$(basename $DIR)
 
+# Clean
+# removing this line from the .uproject forces a rebuild
+sed -i '/EngineAssociation/d' ./$PROJECT.uproject
+
+rm -rf Binaries
+rm -rf DerivedDataCache
+rm -rf Intermediate
+rm -rf Saved
+rm -f CMakeLists.txt
+rm -f Makefile
+
+for d in ./Plugins/*; do
+  if [ -d "$d" ]; then
+    rm -rf $d"/Binaries"
+    rm -rf $d"/DerivedDataCache"
+    rm -rf $d"/Intermediate"
+    rm -rf $d"/Saved"
+  fi
+done
+
+#Build
 $UE_ROOT"/GenerateProjectFiles.sh" -game $DIR"/"$PROJECT".uproject"
 
 make $PROJECT
