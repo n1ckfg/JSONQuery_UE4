e4f65fe85f593c717fc53d6096eb918f9dc1fa81
n1ckfg <nick@fox-gieg.com>
2018-07-30 12:35:35 -0400

    cleanup

--- Source/JSONQuery_UE4/JSONQuery_UE4.Build.cs
+++ Source/JSONQuery_UE4/JSONQuery_UE4.Build.cs
@@ -8,7 +8,7 @@ public class JSONQuery_UE4 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "JSONQuery" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
